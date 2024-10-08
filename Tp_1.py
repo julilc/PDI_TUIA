356b7384d8146773456ac5fe8cc2cb769a813fa8
julilc <julietalopezceratto@gmail.com>
2024-10-08 18:12:59 -0300

    agrego archivo .py

---
+++ Tp_1.py
@@ -0,0 +1,30 @@
+### PDI : Trabajo Práctico N°1 ###
+### Tec. Universitaria en Inteligencia Artificial ###
+### 2024 ###
+
+### Integrantes ###
+# López Ceratto, Julieta : L-3311/1
+# 
+#
+###
+
+#Librerías
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+############ PROBLEMA 1: ECUALIZACIÓN DEL HISTOGRAMA #######
+
+# Carga de imagen
+img_path = './src/imagen_con_detalles_escondidos.tif'
+
+
+######## PROBLEMA 2: CORRECCIÓN DE MULTIPLE CHOICE ########
+
+#Carga de paths
+paths_img = ['imagen_1.png', 'imagen_2.png', 'imagen_3.png', 'imagen_4.png', 'imagen_5.png']
+paths_img = ['./src/'+i for i in paths_img]
+
+
+
+
