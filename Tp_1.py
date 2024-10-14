0f84e23cf83996e1a5d62e29c56f38106291b6b4
julilc <julietalopezceratto@gmail.com>
2024-10-14 13:41:47 -0300

    Termino detección de letras

--- Tp_1.py
+++ Tp_1.py
@@ -97,178 +97,162 @@ paths_img = ['examen_1.png', 'examen_2.png', 'examen_3.png', 'examen_4.png', 'ex
 paths_img = ['./src/'+i for i in paths_img]
 paths_img[0]
 examen = cv2.imread(paths_img[1], cv2.IMREAD_GRAYSCALE)
-plt.figure() , plt.imshow(examen, cmap = 'gray'), plt.show()
+imshow(examen)
+x1 = [20,20,20,20,20,326,326,326,326,326 ]
+x2 = [255,255,255,255,255, 560,560,560,560,560]
+y1 = [59,183,312,440, 564,59,183,312,440, 564 ]
+y2 = [172,296, 421 ,550,671, 172,296, 421 , 550,671 ]
 
-### Corrección de pregunta
-x1 = 20
-x2 = 250
-y1 = 442
-y2 = 550
-pregunta = examen[y1:y2,x1:x2]
-plt.figure() , plt.imshow(pregunta, cmap = 'gray'), plt.show()
+def recortar_preguntas():
+    preguntas = []
+    for i in range(len(x1)):
+        pregunta = examen[y1[i]:y2[i], x1[i]:x2[i]]
+        preguntas.append(pregunta)
+    return preguntas
 
-def encontrar_lineas(img, bordes, r, t, tr):
-    ####
-    # Img : imagen 
-    # bordes : canny de la imagen
-    # r : rhoa
-    # t: theta
-    # tr : treshold
-    ####
+preguntas = recortar_preguntas()
 
-    lineas = cv2.HoughLines(bordes, r,t,tr)
+for pregunta in preguntas:
+    imshow(pregunta)
 
-    img_color = img.copy()
-    img_color = cv2.cvtColor(img_color, cv2.COLOR_GRAY2BGR)
+def binarize(img):
+    _, img_bin = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY_INV)
+    return img_bin
 
-    if lineas is not None:
-        for linea in lineas:
-            rho, theta = linea[0]
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
+def lineas_horizontales(img_bin):
+    # Convertir la imagen en color
+    img_s = cv2.cvtColor(img_bin.copy(), cv2.COLOR_GRAY2BGR)
 
-            cv2.line(img_color, (x1,y1), (x2,y2), (0,0,255), 1)
-    cord_linea = [x1,x2,y1,y2]
-    imshow(img_color)
-    return lineas, cord_linea
+    # Encontrar contornos en la imagen binaria
+    contours, _ = cv2.findContours(img_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-def filtra_bordes(bordes, umbral = 10):
-    ###
-    # bordes: lineas de la imagen
-    # umbral : valor minimo de píxeles que tienen que
-    # tener de distancia una linea de otra.
-    ###
+    # Inicializar variables para el rectángulo más ancho
+    max_width = 0
+    rect_mas_ancho = None
 
-    bordes_filt = []
-
-    bordes = sorted(bordes, key = lambda l:l[0][0])
-
-    for linea in bordes:
-        rho, theta = linea[0]
-
-        if len(bordes_filt) == 0:
-            bordes_filt.append(linea)
-        else:
-            ult_rho = bordes_filt[-1][0][0]
-
-            if abs(rho - ult_rho) > umbral:
-                bordes_filt.append(linea)
-    
-    return bordes_filt
-
-def dividir_pregunta(pregunta, lineas):
-    h , w = pregunta.shape
-    y_max = int(lineas[0][0][0])
-    sub_pregunta = pregunta[0:y_max, 0:w]
-    return sub_pregunta
-
-def componentes_conectadas_pregunta(s_preg, conec: int = 8, min_area : int = 5):
-    
-    # Aplicar umbral binario para binarizar la imagen
-    _, binarizada = cv2.threshold(s_preg, 238, 238, cv2.THRESH_BINARY)
-    
-    # Obtener las componentes conectadas y estadísticas
-    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(binarizada, connectivity=conec, ltype=cv2.CV_32S)
-
-    # Crear una imagen en color para dibujar las componentes conectadas
-    output_image = cv2.cvtColor(binarizada, cv2.COLOR_GRAY2BGR)
-    
-    ix_area= (stats[:, -1] > min_area) & (stats[:, -1] < 40)
-    stats=stats[ix_area,:]
-    centroids = centroids[ix_area, :]
-
-    # Recorremos todas las componentes conectadas (ignorando el fondo)
-    for i in range(1, len(stats)-1):
-        # Obtener las estadísticas para la componente actual
-        x, y, w, h, area = stats[i]
+    for i in range(len(contours)):
+        contour = contours[i]
+        # Obtener el rectángulo delimitador
+        x, y, w, h = cv2.boundingRect(contour)
         
+        # Considerar solo contornos que tengan un cierto alto para evitar ruido
+        if h > 3:  # Ajusta el umbral según sea necesario
+            continue
+        
+        # Comparamos el ancho
+        if w > max_width:  
+            max_width = w
+            rect_mas_ancho = (x, y, w, h)  # Guardamos el rectángulo más ancho
 
-        # Dibujar un rectángulo alrededor de la componente conectada
-        cv2.rectangle(output_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        
-        # Dibujar el centroide de la componente conectada
-        
-        cx, cy = centroids[i]
-        print(cx,cy)
-        cv2.circle(output_image, (int(cx), int(cy)), 1, (255, 0, 0), -1)
+    # Dibujar el rectángulo más ancho si existe
+    if rect_mas_ancho is not None:
+        x, y, w, h = rect_mas_ancho
+        cv2.rectangle(img_s, (x, y), (x + w, y + h), (255, 0, 0), 1)  # Rectángulo en azul
     
-    # Retornar la imagen con las componentes conectadas dibujadas
-    imshow(output_image)
-    return (centroids, stats)
+    # Dibujar los contornos en la imagen
+    cv2.drawContours(img_s, contours, -1, (0, 255, 0), 1)  # Contornos en verde
 
+    # Mostrar la imagen con contornos y el rectángulo más ancho
+    plt.figure(figsize=(10, 6))
+    plt.imshow(cv2.cvtColor(img_s, cv2.COLOR_BGR2RGB))
+    plt.title("Contornos Encontrados y Rectángulo Más Ancho")
+    plt.axis('off')  # Opcional: Oculta los ejes
+    plt.show()
 
-def encontrar_letra(centroids, stats, linea_respuesta):
-    stats_letras = []  # Lista de letras, donde cada letra será una lista de stats
-    x1, x2, y1, y2 = linea_respuesta
-    letra_stat = []  # Lista para almacenar los stats de una letra
-    ult_cx, ult_cy = 0, 0  # Para guardar el último centroide procesado
-    centroids_stats_ord= sorted(zip(centroids, stats), key=lambda cs: cs[0][0])
-    for i, (centroid,stat) in enumerate(centroids_stats_ord):
-        if stat[4]>100:
-            pass
-        else:
-            cx, cy = centroid
-            print(y2)
-            print(cy)
-            print(y2-cy)
-            # Verifica si el componente está dentro de la línea de respuesta y se encuentra suficientemente cerca en altura
-            if (y2 - cy) < 14 and cx > 106:
-                if len(letra_stat) == 0:
-                    # Si es el primer componente de la letra, lo añadimos
-                    letra_stat.append(stat)
-                elif abs(ult_cx - cx) < 10:
-                    # Si el nuevo componente está cerca del anterior, lo añadimos a la lista de stats de la letra actual
-                    letra_stat.append(stat)
-                else:
-                    # Si el componente no está cerca del anterior, guardamos la letra anterior y empezamos una nueva
-                    stats_letras.append(letra_stat)
-                    letra_stat = [stat]  # Empezamos una nueva letra con el componente actual
-                    
-                # Actualizamos el último centroide procesado
-                ult_cx, ult_cy = cx, cy
+    return rect_mas_ancho
+
+def recortar_pregunta(pregunta, linea_preg):
+    x1,y1,w,h = linea_preg
+    h, w = pregunta.shape
+    sub_preg = pregunta[y1-14:y1-2, x1: w]
+    imshow(sub_preg)
+    return sub_preg
+
+def detectar_letra(sub_preg):
+    # Binarizar la imagen
+    sub_bin = binarize(sub_preg)
+    
+    # Encontrar contornos y jerarquía
+    contornos, jerarquia = cv2.findContours(sub_bin, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Crear una imagen en blanco para dibujar los contornos
+    img_contornos = cv2.cvtColor(sub_bin, cv2.COLOR_GRAY2BGR)
+    
+    # Inicializar el diccionario para guardar contornos padres e hijos
+    dict_contornos = {}
+    padres = 0
+    hijos = 0
+    # Dibujar contornos con diferentes colores
+    for i in range(len(contornos)):
+        # Obtener información de jerarquía
+        _, _, first_child, parent = jerarquia[0][i]
         
-    # Si queda una letra en proceso, la añadimos
-    if len(letra_stat) != 0:
-        stats_letras.append(letra_stat)
-
-    return stats_letras
-
-def identificar_letra(letras):
-    for letra in letras:
-        print(letra) 
-        if len(letra) == 0:
-            print('No hay respuesta')
-        if len(letra) == 1:
-                print('c')
-            ### Ver si es C
-        if len(letra) == 3:
-            print('B')
+        if parent == -1:  # Contorno padre
+            padres += 1
+            color = (0, 0, 255)  # Rojo para contornos padres
+            dict_contornos[i] = []  # Inicializar la lista de hijos para este padre
+        else:  # Contorno hijo
+            hijos += 1
+            color = (0, 255, 0)  # Verde para contornos hijos
             
+            # Agregar este contorno hijo a la lista del contorno padre
+            if parent in dict_contornos:
+                dict_contornos[parent].append(i)
 
+        cv2.drawContours(img_contornos, contornos, i, color, 2)  # Dibujar contornos con el color apropiado
 
-pregunta_canny = cv2.Canny(pregunta, threshold1=0, threshold2=1000)
-imshow(pregunta_canny)
-lineas_h, cord_linea = encontrar_lineas(pregunta, pregunta_canny, 1, np.pi / 2, tr = 110)
-lineas_h = filtra_bordes(lineas_h, umbral = 10)
-sub_preg = dividir_pregunta(pregunta, lineas_h)
-sub_preg_canny = cv2.Canny(sub_preg, threshold1= 150, threshold2=190)
-imshow(sub_preg)
-cen, stats = componentes_conectadas_pregunta(sub_preg_canny, conec= 8, min_area= 8)
-letra = encontrar_letra(cen,stats, cord_linea)
-len(letra)
-len(letra[1])
-identificar_letra(letra)
+    # Mostrar la imagen con contornos dibujados
+    print(f' padres: {padres}, hijos : {hijos}')
+    plt.figure(figsize=(10, 6))
+    plt.imshow(cv2.cvtColor(img_contornos, cv2.COLOR_BGR2RGB))
+    plt.title("Contornos de Letras Detectados")
+    plt.axis('off')
+    plt.show()
 
+    return dict_contornos, contornos
 
+def clasificar_letra(dict_contornos, contornos):
+    respuestas = []
+    if len(dict_contornos) == 0:
+        return respuestas
+    else:
+        for key, value in dict_contornos.items():
+            if cv2.contourArea(contornos[key]) > 2:
+                if len(value) == 0:
+                    respuestas.append('C')
+                elif len(value) == 2:
+                    respuestas.append('B')
+                elif  len(value) == 1:
+                    area_hijo = cv2.contourArea(contornos[value[0]])
+                    area_padre = cv2.contourArea(contornos[key])
+                    proporcion = area_hijo / area_padre
+                    if proporcion < 0.8:
+                        respuestas.append('A')
+                    else:
+                        respuestas.append('D')
+    return respuestas
+                
+def corregir_examen(respuesta, i, respuestas_correctas):
+    if len(respuesta) == 0:
+        return 'No hay respuesta'
+    elif len(respuesta) > 1:
+        return 'Contesto mas de una letra'
+    else:
+        if respuesta[0] == respuestas_correctas[i]:
+            return 'Bien'
+        else:
+            return 'Mal'
 
-pregunta_canny.shape
-imshow(pregunta)
-_, binarizada = cv2.threshold(sub_preg, 238, 238, cv2.THRESH_BINARY)
-imshow(binarizada)
+respuestas_corrctas = ['B', 'B', 'D', 'B', 'B', 'A', 'B', 'D', 'D']
+
+for i in range(len(preguntas)):
+    pregunta = preguntas[i]
+    preg_bin = binarize(pregunta)
+    linea_preg = lineas_horizontales(preg_bin)
+    sub_preg = recortar_pregunta(pregunta,linea_preg)
+    dic_letras, cont = detectar_letra(sub_preg)
+    respuestas = clasificar_letra(dic_letras, cont)
+    print(respuestas)
+    corregir_examen(respuestas, i, respuestas_corrctas)
+
 
